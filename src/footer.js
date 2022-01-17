bba2c38548c4da6d29ceb42b762428d18cf55685
Ehsan Mohamed <ehsanmohamed1999@gmail.com>
2022-01-17 15:22:25 +0200

    footer

--- src/footer.js
+++ src/footer.js
@@ -1,33 +1,33 @@
-// import React from 'react';
-// import './footer.css';
-// // import 'font-awesome/css/font-awesome.min.css';
+import React from 'react';
+import './footer.css';
+// import 'font-awesome/css/font-awesome.min.css';
 
-// //class component  >>>>lifecycle hooks  , state
+//class component  >>>>lifecycle hooks  , state
 
-// function Footer() {
-//     return (
-//       <>
+function Footer() {
+    return (
+      <>
       
-//       <div class="container-fluid bg-dark text-light ">
-//   <div class="row ">
-//     <div class="col-3">
-//     <p ><i className="fas fa-envelope"></i> kate@hrhgh.com</p>
-//     </div>
-//     <div class="col-3">
-//     <p className="display-6">About me</p>
-//     </div>
-//     <div class="col-3">
-//     <p className="display-6">About me</p>
-//     </div>
+      <div class="container-fluid bg-dark text-light ">
+  <div class="row ">
+    <div class="col-3">
+    <p ><i className="fas fa-envelope"></i> kate@hrhgh.com</p>
+    </div>
+    <div class="col-3">
+    <p className="display-6">About me</p>
+    </div>
+    <div class="col-3">
+    <p className="display-6">About me</p>
+    </div>
     
-//   </div>
-// </div>
+  </div>
+</div>
 
       
       
-//       </>
-//     );
-//   }
+      </>
+    );
+  }
 
 
-// export default Footer;
+export default Footer;
